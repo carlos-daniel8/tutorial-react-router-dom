61c83dd13b03d852b5063beada3b7c020d3b99e5
Carlos Daniel <carlosdevops22@gmail.com>
2024-05-04 16:20:52 -0300

    Search Params

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable prefer-const */
 import "./styles.css";
-import { NavLink, Outlet } from "react-router-dom";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
 
 export default function Invoices() {
   // eslint-disable-next-line prefer-const
   let invoices = getInvoices();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: "flex" }}>
@@ -14,17 +19,39 @@ export default function Invoices() {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "dblock nav-red" : "dblock nav-blue"
+        <input
+          value={searchParams.get("name") || ""}
+          onChange={(event) => {
+            let name = event.target.value;
+            if (name) {
+              setSearchParams({ name });
+            } else {
+              setSearchParams({});
             }
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            let name = searchParams.get("name");
+            if (!name) {
+              return true;
+            }
+            let invoiceName = invoice.name.toLowerCase();
+            return invoiceName.startsWith(name.toLowerCase());
+          })
+
+          .map((invoice) => (
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "dblock nav-red" : "dblock nav-blue"
+              }
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
